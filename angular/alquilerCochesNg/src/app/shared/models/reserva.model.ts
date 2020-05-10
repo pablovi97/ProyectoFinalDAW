c866f963f4ba67fa247648e1e9dd4d8906d371bd
Pablo Viera <32093629+pablovi97@users.noreply.github.com>
2020-05-10 20:17:03 +0100

    dfg
    dfg

--- angular/alquilerCochesNg/src/app/shared/models/reserva.model.ts
+++
@@ -1,9 +0,0 @@
-import { Detallepedido } from './detallepedido.model';
-export class Reserva {
-    public idReserva: number
-    public fkUsuarioRes: number
-    public detallesPedidos : Array<Detallepedido>
-    constructor(){
-
-    }
-}
